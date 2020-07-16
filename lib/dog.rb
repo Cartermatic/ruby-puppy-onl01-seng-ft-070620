06f9c8f4e422f50a78a4b846097ca77ea69eafc1
Carter Parrett <cparrett94@hotmail.com>
2020-07-16 00:17:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,7 @@
-# Add your code here
+# write a dog class 
+# initialize it with a name 
+#   it also stores each new dog
+# create class variable @@all = empty array 
+# every dog that is initialized gets put in here
+# use self keyword inside initialize to refer to the dog you are pushing into array 
+
